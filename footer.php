78f0793b85159c6cdf2d0b3fe5238d5b99b6c251
Sjoerd <71407166+FailedInCoding@users.noreply.github.com>
2022-11-23 13:48:36 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="content">
+<footer class="FooterAlignment"><p>&copy; Carpie, 2022</p></footer>
+</div>
---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+		<nav>
+		<div class="navigation-bar">
+			<div id="navigation-container">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="hobbies.php">Mijn hobbies</a></li>
+			<li><a href="projects.php">Mijn projecten</a></li>
+			<li><a href="playlist.php">Muziek playlist</a></li>
+			<li><a href="aboutme.php">Over mij</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+		</nav>
+</header>
